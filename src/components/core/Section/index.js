5ac626569dee3af078a5c01a5602c721f557b196
Marco Antônio <ma.souza.junior@gmail.com>
2020-02-05 00:02:08 -0300

    FEA: add Section

---
+++ src/components/core/Section/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+import { colors } from "helpers/theme"
+
+const Section = styled.section`
+  height: 90vh;
+  width: 100%;
+  background-color: ${colors.secondary};
+`
+
+export default Section
