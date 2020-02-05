738ad6fd22718da5bd15c7f9fa57844cec1595c9
Marco Antônio <ma.souza.junior@gmail.com>
2020-02-05 00:04:05 -0300

    ENH: change home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,26 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import PostGrid from "../components/PostGrid"
+import Container from "@components/core/Container"
+import Section from "@components/core/Section"
+import PostGrid from "@components/PostGrid"
+import Layout from "@components/Layout"
+import Image from "@components/image"
+import SEO from "@components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <Section>
+      <Container>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+      </Container>
+    </Section>
 
-    <PostGrid />
+    <Container>
+      <PostGrid />
+    </Container>
   </Layout>
 )
 
