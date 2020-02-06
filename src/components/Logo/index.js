ef7df43419f86037edaf069072ca69ad16347619
Marco Antônio <ma.souza.junior@gmail.com>
2020-02-06 14:19:15 -0300

    ENH: add images to logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,24 +1,27 @@
 import React from "react"
+
 import LogoWhite from "images/assets/logo-white.svg"
 import LogoBlack from "images/assets/logo-black.svg"
 import LogoCleanWhite from "images/assets/logo-clean-white.svg"
 import LogoCleanBlack from "images/assets/logo-clean-black.svg"
-import LogoTextWhite from "images/assets/logo-text-white.svg"
-import LogoTextBlack from "images/assets/logo-text-black.svg"
+import { useImages } from "hooks/images"
 
-import { LogoContainer } from "./styled"
+import { LogoContainer, Image } from "./styled"
 
 const Logo = ({ darkMode, small, ...props }) => {
   const LogoIcon = darkMode ? LogoWhite : LogoBlack
-  const LogoText = darkMode ? LogoTextWhite : LogoTextBlack
   const LogoClean = darkMode ? LogoCleanWhite : LogoCleanBlack
+  const logoText = `logo-text-${darkMode ? "white" : "black"}`
+
+  const { getImage } = useImages()
+  const logoImage = getImage(logoText)
 
   if (small) return <LogoClean />
 
   return (
     <LogoContainer>
-      <LogoIcon height={80} width={80} />
-      <LogoText height={80} width={200} />
+      <LogoIcon height={120} width={120} />
+      <Image fluid={logoImage.fluid} />
     </LogoContainer>
   )
 }
--- src/components/Logo/styled.js
+++ src/components/Logo/styled.js
@@ -1,6 +1,14 @@
 import { Fragment } from "react"
 import styled from "styled-components"
+import Img from "gatsby-image"
 
-export const LogoContainer = styled(Fragment)`
+export const LogoContainer = styled.div`
   display: flex;
+  align-items: center;
+
+  .gatsby-image-wrapper {
+    width: 200px;
+  }
 `
+
+export const Image = styled(Img)``
