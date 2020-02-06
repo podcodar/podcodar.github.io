b61e570e2588cbeff18f96bac7fbbc476c569719
Marco Antônio <ma.souza.junior@gmail.com>
2020-02-06 15:36:44 -0300

    ENH: improve colors

--- src/helpers/theme.js
+++ src/helpers/theme.js
@@ -3,7 +3,8 @@ export const colors = {
   secondary: "rebeccapurple",
 
   "text-primary": "darkgrey",
-  "text-secondary": "white",
+  "text-secondary": "#FFFFFF",
 
-  white: "white",
+  white: "#FFFFFF",
+  black: "#36393f",
 }
