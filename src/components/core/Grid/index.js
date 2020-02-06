fb62dc8624b2cdf08b70a16a551c440378d10cae
Marco Antônio <ma.souza.junior@gmail.com>
2020-02-06 20:57:26 -0300

    ENH: use dynamic gap in grid

--- src/components/core/Grid/index.js
+++ src/components/core/Grid/index.js
@@ -1,9 +1,11 @@
 import styled from "styled-components"
 
+const getGap = props => props.gap || "0px"
+
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, auto);
-  grid-gap: 8px 8px;
+  grid-gap: ${getGap} ${getGap};
 `
 
 Grid.Item = styled.div``
