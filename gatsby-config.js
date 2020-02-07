2ab860f77dc0adeda35efddc3f9661e293d37a63
Marco Antônio <ma.souza.junior@gmail.com>
2020-02-07 17:19:29 -0300

    ENH: change icon image

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/post-mark.png`, // This path is relative to the root of the site.
+        icon: `src/images/assets/logo-black.svg`, // This path is relative to the root of the site.
       },
     },
     // handle mdx import
