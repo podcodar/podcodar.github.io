ed1255be92f8f9bde72043ef7e3508468a330f11
Marco Antônio <ma.souza.junior@gmail.com>
2020-02-07 16:04:06 -0300

    ENH: make cover styles more stable

--- src/components/core/PostCover/styled.js
+++ src/components/core/PostCover/styled.js
@@ -7,8 +7,8 @@ export const Container = styled.a`
   color: ${colors.white};
   text-decoration: none;
 `
-export const Title = styled.h1`
-  font-size: 4em;
+export const Title = styled.h2`
+  /* font-size: 4em; */
 `
 
 export const SubTitle = styled.h5`
@@ -17,15 +17,22 @@ export const SubTitle = styled.h5`
   margin-bottom: 4em;
 `
 
-export const Episode = styled.h6`
+export const Episode = styled.h1`
   color: ${colors.white};
+`
+
+export const Duration = styled.h5`
+  color: ${colors.white};
+  left: 2em;
+  bottom: -150px;
+  position: absolute !important;
+  opacity: 0.6;
 `
 
 export const Image = styled(Img)`
   right: 0;
-  overflow: hidden;
   width: 120px;
-  bottom: -100px;
+  bottom: -150px;
   position: absolute !important;
   opacity: 0.6;
 `
