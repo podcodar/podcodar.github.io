1e292ebefadf18fece586d6d684a8abb888f1a4d
Marco Antônio <ma.souza.junior@gmail.com>
2020-02-07 08:01:36 -0300

    ENH: Make grid col same size

--- src/components/core/Grid/index.js
+++ src/components/core/Grid/index.js
@@ -4,7 +4,7 @@ const getGap = props => props.gap || "0px"
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, auto);
+  grid-template-columns: repeat(3, 1fr);
   grid-gap: ${getGap} ${getGap};
 `
 
