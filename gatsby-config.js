72347f02608d5bca4332ca53c3749edd1accdbfb
Marco Antônio <ma.souza.junior@gmail.com>
2020-03-04 22:52:18 -0300

    FIX: remove anonymize

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,9 +73,9 @@ module.exports = {
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
-        anonymize: true,
+        // anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        // respectDNT: true,
         // Avoids sending pageview hits from custom paths
         exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
