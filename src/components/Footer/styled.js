671666c788265f169a43d92ffc85ab317cedd8b4
Marco Antônio <marco@paradigm.co>
2021-03-03 13:43:30 -0300

    ref: remove everything

--- src/components/Footer/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-import CoreContainer from "@components/core/Container"
-
-export const Container = styled(CoreContainer)`
-  padding: 4em 0;
-  text-align: center;
-`
