0f7d587d0219a29fc16545ced711178e924a2890
Marco Antônio <marco@paradigm.co>
2021-03-04 13:19:06 -0300

    enh: split app layout components

--- components/LandingPageLayout.tsx
+++ components/LandingPageLayout.tsx
@@ -1,39 +1,25 @@
-import { Layout, Menu } from 'antd'
+import { Layout } from 'antd'
 import { ReactNode } from 'react'
-import Image from 'next/image'
+import Footer from '#/components/Footer';
+import TopMenu from '#/components/TopMenu';
 
 interface LandingPageLayoutProps {
   children: ReactNode;
 }
-const { Header, Content, Footer } = Layout;
+const { Content } = Layout;
 
 export default function LandingPageLayout({
   children,
 }: LandingPageLayoutProps) {
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Header style={{ display: 'flex' }}>
-        <div style={{ flex: 1 }} >
-          <Image
-            unoptimized
-            src="/images/logo-with-name.png"
-            alt="Picture of the author"
-            width={120}
-            height={60}
-          />
-        </div>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">Home</Menu.Item>
-          <Menu.Item key="2">Como funciona?</Menu.Item>
-          <Menu.Item key="3">Faça Parte!</Menu.Item>
-        </Menu>
-      </Header>
+      <TopMenu />
 
-      <Content style={{ padding: '0 50px' }}>
+      <Content>
         {children}
       </Content>
 
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      <Footer />
     </Layout>
   )
 }
---
+++ components/TopMenu.tsx
@@ -0,0 +1,50 @@
+import { Layout, Menu } from 'antd'
+import Image from 'next/image'
+import React, { useState } from 'react'
+
+const menuItems = [
+  "Home",
+  "Como Funciona?",
+  "Faça Parte!",
+]
+
+function Logo() {
+  return (
+    <Image
+      unoptimized
+      src="/images/logo-with-name.png"
+      alt="Picture of the author"
+      width={120}
+      height={50}
+    />
+  )
+}
+
+
+export default function TopMenu() {
+  const [selectedKey, setSelectedKey] = useState('1')
+  const handleClick = (key: string) =>
+    () => setSelectedKey(key)
+  return (
+    <Layout.Header className="navbar">
+      <div className="container">
+        <div style={{ flex: 1 }} >
+          <Logo />
+        </div>
+
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={[selectedKey]}
+          className="navbar-items"
+        >
+          {menuItems.map(item => (
+            <Menu.Item key={item} onClick={handleClick(item)} >
+              {item}
+            </Menu.Item>
+          ))}
+        </Menu>
+      </div>
+    </Layout.Header>
+  )
+}
